903ad014a9ef39d70f0e03b87a88db13d6e9e81e
akaics <gulsumerd38@gmail.com>
2023-09-15 23:41:32 +0200

    Add project files.

---
+++ Para4/Dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Para4
+{
+    public class Dog
+    {
+        List<Dog> dogs = new List<Dog>();
+
+        public string Name { get; set; }
+
+        public Dog(string name)
+        {
+            Name = name;
+            dogs = new List<Dog>();
+        }
+
+        //Add dog method
+
+        public void AddDog(Dog dog) 
+        {
+            dogs.Add(dog);
+        }
+    }
+}
---
+++ Para4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Para4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dog dog1 = new Dog("Kara");
+            Dog dog2 = new Dog("Boncuk");
+            Dog dog3 = new Dog("Velo");
+
+        }
+    }
+}
